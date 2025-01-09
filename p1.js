84233fa020a6a0d9f1ab2c0d5d930ffc4cc87fc2
lordsainath <lordsainath@gmail.com>
2025-01-09 23:28:04 +0530

    solved Q4 and Q5

--- p1.js
+++ p1.js
@@ -1,4 +1,5 @@
-document.getElementById("heading").addEventListener("click", function () {
+//Q1. Select an element with the id `header` and change its text content to "Welcome to My Site".
+document.getElementById("main").addEventListener("click", function () {
   setInterval(() => {
     this.textContent = "Welcome to my site";
   }, 1000);
@@ -9,12 +10,9 @@ document.getElementById("heading").addEventListener("click", function () {
 //   }, 1000);
 
 
-//   var c1 = document.getElementsByClassName('boxitems').addEventLister('click',function(){
-//     console.log("hello")
-//   })
 
 
-
+// Q2. Select all elements with the class `item` and change their background color to yellow
 document.querySelector(".btnitems").addEventListener('click',function(e){
 
      if(e.target.id === "btnid1"){
@@ -26,7 +24,23 @@ document.querySelector(".btnitems").addEventListener('click',function(e){
      else if(e.target.id === "btnid3") {
        document.querySelector(".box-container").style.backgroundColor = "goldenrod"
      }
-           
-    
-     
+          
 })
+
+
+// Q3.  Add a new class `highlight` to the element with the id `main`.
+document.getElementById('main').classList.add("highlight");
+document.getElementById('main').id = "newid";
+
+// Q4. Remove the class `inactive` from all elements with the class `menu-item`.
+var dom1 = document.querySelectorAll('.btn')
+dom1.forEach(function(item){
+  item.classList.remove('inactive')
+})
+
+// Q5. Retrieve and log the text content of the first `<p>` element inside a container with the class `article`.
+var tc = document.querySelector('p').textContent
+var element = document.createElement('p')
+element.textContent = tc
+document.querySelector('body').appendChild(element)
+
